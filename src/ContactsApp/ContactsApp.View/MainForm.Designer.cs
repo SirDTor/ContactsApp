8bc3aac6ba91371b7f402c2e9f77adcccbeeec21
SirDTor <daniil.zorin.leon@gmail.com>
2023-03-24 19:06:42 +0700

    UI<MainForm> начата верстка главной формы

--- src/ContactsApp/ContactsApp.View/MainForm.Designer.cs
+++ src/ContactsApp/ContactsApp.View/MainForm.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contactGroupBox = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::ContactsApp.View.Properties.Resources.add_contact_32x32_gray;
+            this.button1.Location = new System.Drawing.Point(40, 500);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 39);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = global::ContactsApp.View.Properties.Resources.edit_contact_32x32_gray;
+            this.button2.Location = new System.Drawing.Point(133, 500);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(43, 39);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Image = global::ContactsApp.View.Properties.Resources.remove_contact_32x32_gray;
+            this.button3.Location = new System.Drawing.Point(227, 500);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(43, 39);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // contactGroupBox
+            // 
+            this.contactGroupBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.contactGroupBox.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.contactGroupBox.Location = new System.Drawing.Point(12, 51);
+            this.contactGroupBox.Name = "contactGroupBox";
+            this.contactGroupBox.Size = new System.Drawing.Size(288, 442);
+            this.contactGroupBox.TabIndex = 4;
+            this.contactGroupBox.TabStop = false;
+            this.contactGroupBox.Text = "Контакты";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.contactGroupBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "ContactsApp";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox contactGroupBox;
     }
 }
 
