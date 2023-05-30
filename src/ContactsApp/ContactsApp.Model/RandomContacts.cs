9322fa126663c7c72665a6ec0212f1f2ccc279f5
zorindaniilleon <daniil.zorin.leon@gmail.com>
2023-05-30 23:19:34 +0700

    docs<Explanatory note> Добавлена документация

--- src/ContactsApp/ContactsApp.Model/RandomContacts.cs
+++ src/ContactsApp/ContactsApp.Model/RandomContacts.cs
@@ -9,51 +9,51 @@ namespace ContactsApp.Model
     /// <summary>
     /// Класс генерации случайных контактов
     /// </summary>
-    public class RandomContacts
+    public static class RandomContacts
     {
         /// <summary>
         /// Поле класса <see cref="Project">
         /// </summary>
-        private static Project _project = new Project();
+        private static Project project = new Project();
 
         /// <summary>
         /// Переменная для генерации случайного числа
         /// </summary>
-        private static Random _randomNumber = new Random();
+        private static Random randomNumber = new Random();
 
         /// <summary>
         /// Поле фамилий
         /// </summary>
-        private static string[] _surnames = {"Смирнов","Долгов","Беспалов",
+        private static string[] surnames = {"Смирнов","Долгов","Беспалов",
             "Малышев","Толкачев", "Лазарев","Яковлев","Демин","Журавлев","Кондрашов" };
 
         /// <summary>
         /// Поле мужских имен
         /// </summary>
-        private static string[] _maleNames = { "Тимур", "Михаил", "Иван", "Дмитрий",
+        private static string[] maleNames = { "Тимур", "Михаил", "Иван", "Дмитрий",
             "Николай", "Тимофей" };
 
         /// <summary>
         /// Поле женских имен
         /// </summary>
-        private static string[] _femaleNames = { "Амбразура", "София", "Анна",
+        private static string[] femaleNames = { "Амбразура", "София", "Анна",
             "Полина", "Елизавета", "Александра", "Анна" };
 
         /// <summary>
         /// Поле отчеств
         /// </summary>
-        private static string[] _patronymics = {"Амбразуров","Матвеев","Михайлов","Егоров",
+        private static string[] patronymics = {"Амбразуров","Матвеев","Михайлов","Егоров",
             "Алексеев","Григорьев","Александров","Глебов", "Даниилов" };
 
         /// <summary>
         /// Поле ФИО
         /// </summary>
-        private static string[] _fullNames = new string[20];
+        private static string[] fullNames = new string[20];
 
         /// <summary>
         /// Поле Email
         /// </summary>
-        private static string[] _emails = { "o@outlook.com","hr6zdl@yandex.ru",
+        private static string[] emails = { "o@outlook.com","hr6zdl@yandex.ru",
             "kaft93x@outlook.com","dcu@yandex.ru","19dn@outlook.com","pa5h@mail.ru",
             "281av0@gmail.com","8edmfh@outlook.com","sfn13i@mail.ru","g0orc3x1@outlook.com",
             "rv7bp@gmail.com","93@outlook.com", "er@gmail.com","o0my@gmail.com",
@@ -63,14 +63,14 @@ namespace ContactsApp.Model
         /// <summary>
         /// Поле номеров телефонов
         /// </summary>
-        private static string[] _phones = { "8(923)480-65-83","8(923)234-84-20","8(923)136-38-75",
+        private static string[] phones = { "8(923)480-65-83","8(923)234-84-20","8(923)136-38-75",
             "8(923)653-03-02","8(923)824-41-77","8(923)112-54-20","8(923)678-54-33",
             "8(923)971-37-41", "8(923)760-83-72","8(923)950-38-18"};
 
         /// <summary>
         /// Поле idVK
         /// </summary>
-        private static string[] _idVk = { "id8626810", "id2307938", "id7027388", "id6938923",
+        private static string[] idVk = { "id8626810", "id2307938", "id7027388", "id6938923",
             "id2570718", "id7381412", "id1153817", "id7013733", "id8111215", "id6492395" };
 
 
@@ -84,7 +84,7 @@ namespace ContactsApp.Model
         /// </returns>
         private static int GenerateDigit(int lengthOfArray)
         {
-            return _randomNumber.Next(lengthOfArray);
+            return randomNumber.Next(lengthOfArray);
         }
 
         /// <summary>
@@ -94,31 +94,31 @@ namespace ContactsApp.Model
         /// <param name="randomContacts"></param>
         public static List<Contact> GenerateRandomContactsName()
         {
-            _project.Contacts.Clear();
+            project.Contacts.Clear();
             for (int i = 0; i <= 10; i++)
             {
-                _fullNames[i] = _surnames[GenerateDigit(_surnames.Length)] + " " +
-                    _maleNames[GenerateDigit(_maleNames.Length)] + " "
-                    + _patronymics[GenerateDigit(_patronymics.Length)] + "ич";
+                fullNames[i] = surnames[GenerateDigit(surnames.Length)] + " " +
+                    maleNames[GenerateDigit(maleNames.Length)] + " "
+                    + patronymics[GenerateDigit(patronymics.Length)] + "ич";
 
             }
             for (int i = 11; i < 20; i++)
             {
-                _fullNames[i] = _surnames[GenerateDigit(_surnames.Length)] + "а " +
-                    _femaleNames[GenerateDigit(_femaleNames.Length)] + " "
-                    + _patronymics[GenerateDigit(_patronymics.Length)] + "на";
+                fullNames[i] = surnames[GenerateDigit(surnames.Length)] + "а " +
+                    femaleNames[GenerateDigit(femaleNames.Length)] + " "
+                    + patronymics[GenerateDigit(patronymics.Length)] + "на";
 
             }
             for (int i = 0; i < 20; i++)
             {
-                Contact contact = new Contact(_fullNames[GenerateDigit(_fullNames.Length)],
-                _emails[GenerateDigit(_emails.Length)],
-                    _phones[GenerateDigit(_phones.Length)], DateTime.Today,
-                    _idVk[GenerateDigit(_idVk.Length)]);
+                Contact contact = new Contact(fullNames[GenerateDigit(fullNames.Length)],
+                emails[GenerateDigit(emails.Length)],
+                    phones[GenerateDigit(phones.Length)], DateTime.Today,
+                    idVk[GenerateDigit(idVk.Length)]);
 
-                _project.Contacts.Add(contact);
+                project.Contacts.Add(contact);
             }
-            return _project.Contacts;
+            return project.Contacts;
         }
     }
 }
